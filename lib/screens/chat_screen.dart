50b4daffc643035c6a3a6b46622377b1b6e4629a
Jo-HyeRin <sayhyelin12@naver.com>
2022-11-11 13:49:11 +0900

    5 채팅 리스트 화면 만들기

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_kakao/components/chat_card.dart';
+import 'package:flutter_kakao/models/chat.dart';
 
 // 채팅방 목록 보기
 class ChatScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("ChatScreen"),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("채팅"), titleTextStyle: TextStyle(color: Colors.black, fontSize: 24),
+      ),
+      body: ListView(
+        children: List.generate(chats.length, (index) => ChatCard(chat: chats[index]),
+        ),
+      ),
     );
   }
 }
