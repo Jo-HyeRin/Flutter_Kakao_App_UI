68940bda8451d63dd562e7b12a66a8a5d66d5b89
Jo-HyeRin <sayhyelin12@naver.com>
2022-11-11 10:42:20 +0900

    3 친구 화면 만들기

--- lib/screens/friend_screen.dart
+++ lib/screens/friend_screen.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_kakao/components/profile_card.dart';
+import 'package:flutter_kakao/models/user.dart';
 
 // 친구 목록 보기
 class FriendScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("FriendScreen"),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(title: Text("친구"), titleTextStyle: TextStyle(color: Colors.black, fontSize: 24),),
+      body: Column(
+        children: [
+          const SizedBox(height: 10),
+          ProfileCard(user: me),
+          Divider(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Row(
+              children: [
+                Text("친구"),
+                const SizedBox(width: 6),
+                Text("${friends.length}"),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: List.generate(friends.length, (index) => ProfileCard(user: friends[index])),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
